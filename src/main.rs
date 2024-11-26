2824509882c02eca2c16a8a8dfceb398c268eb89
Gun on Tae-PC <napat.gunt3001@gmail.com>
2024-11-26 22:44:38 +0700

    [feat] can now query Reddit

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,37 @@
 mod cli;
+mod myucel;
 
 use clap::Parser;
 use cli::Args;
 use colored::Colorize;
 use spinners::{Spinner, Spinners};
-use std::thread::sleep;
-use std::time::Duration;
+use crate::myucel::Myucel;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args = Args::parse();
 
+    // Display a spinner while searching Reddit
     let status_text = format!(
         "Searching Reddit /r/anime for {} discussion threads...",
         args.name.green().bold()
     )
     .blue().to_string();
     let mut spinner = Spinner::new(Spinners::Dots, status_text.into());
-    sleep(Duration::from_secs(3));
-    spinner.stop();
+
+    // Search Reddit
+    match Myucel::search_reddit(&args.name).await {
+        Ok(posts) => {
+            spinner.stop();
+            println!();
+            for post in posts {
+                println!("{}: {}", post.title, post.url);
+            }
+        }
+        Err(e) => {
+            spinner.stop();
+            println!();
+            eprintln!("Error searching Reddit: {}", e);
+        }
+    }
 }
