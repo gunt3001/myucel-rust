6002037a365b7af872f31b835c246062a84e1f19
Gun on Tae-PC <napat.gunt3001@gmail.com>
2024-11-26 22:17:32 +0700

    [feat] display progress spinner

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 mod cli;
 
-use cli::Args;
 use clap::Parser;
+use cli::Args;
+use colored::Colorize;
+use spinners::{Spinner, Spinners};
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
     let args = Args::parse();
-    println!("name: {:?}", args.name);
+
+    let status_text = format!(
+        "Searching Reddit /r/anime for {} discussion threads...",
+        args.name.green().bold()
+    )
+    .blue().to_string();
+    let mut spinner = Spinner::new(Spinners::Dots, status_text.into());
+    sleep(Duration::from_secs(3));
+    spinner.stop();
 }
